cae0448c97370514d071360ec7f795728fc2f18a
Jonatan Jönsson <jontejj@gmail.com>
2023-02-21 17:38:24 +0100

    Baywatch Dreams

--- src/main/java/com/malmoimprov/MyUI.java
+++ src/main/java/com/malmoimprov/MyUI.java
@@ -95,28 +95,28 @@ public class MyUI extends UI
 	private static final Logger LOG = LoggerFactory.getLogger(MyUI.class);
 
 	private static final String CONFIG_KEY_SENDGRID = "SENDGRID";
-	static final long EVENT_ID = 30;
+	static final long EVENT_ID = 31;
 	private static final String CURRENCY = "SEK";
 	private static final String PHONENUMBER_TO_PAY_TO = "0764088570";
-	private static final long initialSeatCapacity = 35;
+	private static final long initialSeatCapacity = 60;
 	private static final BigDecimal ticketPrice = new BigDecimal("60");
 	private static final BigDecimal memberPricePercentage = new BigDecimal("0.666667");
 	// private static final BigDecimal folkUniPricePercentage = new BigDecimal("0.60");
 
-	private static final String facebookEventUrl = "https://www.facebook.com/events/561819722667691/";
+	private static final String facebookEventUrl = "https://www.facebook.com/events/1880496055651957";
 	/**
 	 * https://console.cloud.google.com/storage/browser?folder=&organizationId=&project=malmo-improv
 	 */
-	private static final String imageLink = "https://storage.googleapis.com/malmo-improv.appspot.com/events/Grey-Rainbow.png";
-	static final String eventName = "Grey Rainbow - improv show";
+	private static final String imageLink = "https://storage.googleapis.com/malmo-improv.appspot.com/events/baywatch-dreams.png";
+	static final String eventName = "Baywatch Dreams - Improv Show";
 	private static final com.google.schemaorg.core.Event event = CoreFactory.newTheaterEventBuilder() //
 			.addUrl(facebookEventUrl) //
 			.addName(eventName) //
 			.addImage(imageLink) //
 			.addOrganizer("Malmö Improvisatorium") //
-			.addStartDate("2023-02-10T18:00:00+01:00") //
+			.addStartDate("2023-03-04T18:00:00+01:00") //
 			.addDuration("PT1H30M") //
-			.addLocation(Stages.STAGE_2) //
+			.addLocation(Stages.STAGE_3) //
 			.addProperty("phoneNumber", PHONENUMBER_TO_PAY_TO).build();
 
 	private static final Configuration cfg;
